4a5ffd09546139e93deb7bf452f6f4763d1cb03f
sindhushakoppisetty <96305586+sindhushakoppisetty@users.noreply.github.com>
2023-07-15 12:20:09 +0530

    #include<stdio.h> int main(){     int n,count=0;     scanf("%d",&n);     int a[n];     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);         if(a[i]%2!=0)         count++;     }     if (count>2)     printf("NO");     else     printf("YES");     return 0;
    }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
-int main()
-{
-    int n,x[20],odd=0,i;
+int main(){
+    int n,count=0;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    scanf("%d",&x[i]);
-    for(i=0;i<n;i++)
+    int a[n];
+    for(int i=0;i<n;i++)
     {
-        if(x[i]%2!=0)
-        odd++;
+        scanf("%d",&a[i]);
+        if(a[i]%2!=0)
+        count++;
     }
-    if(odd<=2)
-    printf("YES");
-    else
+    if (count>2)
     printf("NO");
+    else
+    printf("YES");
     return 0;
+
 }
