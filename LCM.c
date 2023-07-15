3dd32a053a9866c12a333df4515d313ef537b3e9
sindhushakoppisetty <96305586+sindhushakoppisetty@users.noreply.github.com>
2023-07-15 12:08:02 +0530

    #include <stdio.h> int main() {     int n1, n2, max;   ;     scanf("%d %d", &n1, &n2);     max = (n1 > n2) ? n1 : n2;     while (1)     {         if (max % n1 == 0 && max % n2 == 0) {             printf("%d",max);             break;         }         ++max;     }     return 0; }

--- LCM.c
+++ LCM.c
@@ -1,17 +1,16 @@
-#include <stdio.h>  
-int main()  
-{  
-    int num1,num2,max_div, flag = 1;  
-    scanf(" %d %d", &num1, &num2);  
-    max_div = (num1 > num2) ? num1 : num2;  
-    while (flag)  
-    {  
-        if (max_div % num1 == 0 && max_div % num2 == 0)  
-        {  
-            printf( "%d",max_div);  
-            break;  
-        }  
-        ++max_div;
+#include <stdio.h>
+int main() {
+    int n1, n2, max;
+  ;
+    scanf("%d %d", &n1, &n2);
+    max = (n1 > n2) ? n1 : n2;
+    while (1) 
+    {
+        if (max % n1 == 0 && max % n2 == 0) {
+            printf("%d",max);
+            break;
+        }
+        ++max;
     }
     return 0;
 }
